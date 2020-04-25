15d637f58f8cbaae804b5fc94835d79e6d8105eb
VladimirB <vladimirzq@gmail.com>
2020-04-25 23:28:33 +0600

    added header comp

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -4,17 +4,6 @@ import CollectionItem from '../collection-item/collection-item.component';
 
 const CollectionPreview = ({ title, items }) => {
   return (
-    // <div className={`collection-preview-item`}
-    //     onClick={() => history.push(`${match.url}${linkUrl}`)}>
-    //   <div className='background-image' style={{
-    //     backgroundImage: `url(${imageUrl})`
-    //    }}/>
-    //   <div className='content'>
-    //     <h1 className='title'>{title.toUpperCase()}</h1>
-    //     <span className='subtitle'>SHOP NOW</span>
-    //   </div>
-    // </div>
-
     <div className="collection-preview">
       <h1 className="title">{title.toUpperCase()}</h1>
       <div className="preview">
